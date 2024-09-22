e3468cbdadfd295819789c0ace1e657dea5f6edf
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-09-22 11:49:26 -0400

    style active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { GiAlienBug } from "react-icons/gi";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {
       label: "Dashbord",
@@ -23,7 +28,11 @@ const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors">
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}>
             {link.label}
           </Link>
         ))}
