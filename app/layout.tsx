e398b6500b1e5ab7ed92cfd076da3690c68dfa85
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-09-22 14:06:46 -0400

    customize radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css'
-import type { Metadata } from 'next'
+import type { Metadagita } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from "./NavBar";
 import "@radix-ui/themes/styles.css";
-import { Theme } from '@radix-ui/themes';
+import { Theme } from "@radix-ui/themes";
+import './theme-config.css'
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: 'ResolveMe.app',
@@ -19,11 +19,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body className={inter.className}>
-        <Theme>
+        <Theme appearance="light" accentColor="violet">
           <NavBar />
-          <main className='p-5'>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
