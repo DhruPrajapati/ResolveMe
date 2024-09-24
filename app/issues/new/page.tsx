25c6c9d6fbd8ec6198d1468b2a71295100fe9868
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-09-24 17:03:13 -0400

    show the issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -26,6 +26,16 @@ const NewIssuePage = () => {
   });
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error Occured");
+    }
+  });
 
   return (
     <div className="max-w-xl ">
@@ -34,18 +44,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className=" space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An unexpected error Occured");
-          }
-        })}>
+      <form className=" space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,46 @@
-import { Button } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>{" "}
-      </Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>{" "}
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issues) => (
+            <Table.Row key={issues.id}>
+              <Table.Cell>
+                {issues.title}
+                <div className="block md:hidden">{issues.Status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issues.Status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issues.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
