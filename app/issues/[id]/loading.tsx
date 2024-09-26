d84434a762be81f8fa6027f299d4c57e7a9d5563
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-09-26 17:11:54 -0400

    add issue detail page

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingIssueDetail = () => {
+  return <div>loading ....</div>;
+};
+
+export default LoadingIssueDetail;
---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,34 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+  //   if (typeof params.id !== "number") notFound();
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <Heading> {issue.title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge Status={issue.Status} />
+        <Text>{issue.createdAt.toDateString()} </Text>
+      </Flex>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
