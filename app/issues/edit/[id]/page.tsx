bd559b233a6591c8681860e58eec3137649b8cff
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-10-13 16:38:40 -0400

    edit Page of Id

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -1,8 +1,8 @@
+import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
-import { Issue } from "@prisma/client";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
@@ -10,36 +10,16 @@ const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
 });
 
 interface Props {
-  issue: Issue;
+  params: { id: string };
 }
 
-export async function getStaticPaths() {
-  const issues = await prisma.issue.findMany();
-  const paths = issues.map((issue) => ({
-    params: { id: issue.id.toString() },
-  }));
-
-  return {
-    paths,
-    fallback: "blocking", // Ensures the page waits for the issue data
-  };
-}
-
-export async function getStaticProps({ params }: { params: { id: string } }) {
+const EditIssuePage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
-  if (!issue) {
-    return { notFound: true };
-  }
+  if (!issue) notFound();
 
-  return {
-    props: { issue },
-  };
-}
-
-const EditIssuePage = ({ issue }: Props) => {
   return <IssueForm issue={issue} />;
 };
 
