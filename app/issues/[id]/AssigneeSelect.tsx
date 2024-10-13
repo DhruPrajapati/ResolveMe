c38b0dfe443f2abe474273e33f16d31edb946a17
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-10-13 14:05:51 -0400

    fixing error with placeholder

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -29,7 +29,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
       <Select.Root
         defaultValue={issue.assignedToUserId || ""}
         onValueChange={assignIssue}>
-        <Select.Trigger />
+        <Select.Trigger placeholder="Assignee..." />
         <Select.Content>
           <Select.Group>
             <Select.Label>Suggestions</Select.Label>
